783abce0cccc0adead30797618267ca06420fc8f
Michael <mikahilyurchenko2601@gmail.com>
2020-04-16 15:25:17 +0300

    Added ORM SQLALCHEMY_FLASK

--- application.py
+++ application.py
@@ -1,39 +1,39 @@
-import os
-
+from models import *
 from flask import Flask, render_template, request, redirect, url_for
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:Yurchenko26@localhost:5432/lecture3"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
 
-engine = create_engine("postgresql://postgres:Yurchenko26@localhost:5432/lecture3")
-db = scoped_session(sessionmaker(bind=engine))
+
 
 @app.route("/")
 def index():
-    flights = db.execute("SELECT * FROM flights ORDER BY id ASC").fetchall()
+    flights = Flight.query.all()
     return render_template("index.html", flights=flights)
 
 @app.route("/flight/<int:flight_id>", methods=["POST", "GET"])
 def flight(flight_id):
-    flight = db.execute("SELECT * FROM flights WHERE id=:id", {"id":flight_id}).fetchone()
+    flight = Flight.query.get(flight_id)
 
     if flight is None:
         return render_template("error.html", message="No such flight")
 
-    passengers = db.execute("SELECT name FROM passengers WHERE flight_id=:id", {"id":flight_id}).fetchall()
+    passengers = Passenger.query.filter_by(flight_id = flight_id).all()
     if request.method == "POST":
         name = request.form.get("name")
-        db.execute("INSERT INTO passengers(name, flight_id) VALUES(:name, :flight_id)", {"name":name, "flight_id":flight_id})
-        db.commit()
+        passenger = Passenger(name = name, flight_id = flight_id)
+        db.session.add(passenger)
+        db.session.commit()
         return redirect(f"/flight/{flight_id}")
 
     return render_template("flight.html", flight=flight, passengers=passengers)
 
 @app.route("/register")
 def register():
-    flights = db.execute("SELECT * FROM flights").fetchall()
+    flights = Flight.query.all()
     return render_template("register.html", flights=flights)
 
 @app.route("/book", methods=["POST"])
@@ -44,6 +44,8 @@ def book():
     except ValueError:
         return render_template("error.html", message="Wrong flight id")
 
-    db.execute("INSERT INTO passengers(name, flight_id) VALUES(:name, :flight_id)", {"name":name, "flight_id":flight_id})
-    db.commit()
+    passenger = Passenger(name = name, flight_id = flight_id)
+    db.session.add(passenger)
+    db.session.commit()
+
     return redirect(f"/flight/{flight_id}")
